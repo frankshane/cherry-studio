193e30d5193e689d34762041a62ad4c6896759f5
suyao <sy20010504@gmail.com>
2025-08-10 17:26:44 +0800

    feat: add MdiDragHandle icon and update RichEditor to use it
    - Introduced a new MdiDragHandle SVG icon in the SVGIcon component.
    - Replaced the MdiLightbulbOn icon with MdiDragHandle in the RichEditor component for improved functionality.

--- src/renderer/src/components/Icons/SVGIcon.tsx
+++ src/renderer/src/components/Icons/SVGIcon.tsx
@@ -112,3 +112,17 @@ export function MdiLightbulbOn(props: SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
+export function MdiDragHandle(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      {...props}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+    </svg>
+  )
+}
