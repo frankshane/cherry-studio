f73850798c2089474f2f160ef3712a4314c615e3
eeee0717 <chentao020717Work@outlook.com>
2025-08-11 14:58:51 +0800

    fix: migration error

--- src/renderer/src/hooks/useKnowledgeBaseForm.ts
+++ src/renderer/src/hooks/useKnowledgeBaseForm.ts
@@ -4,7 +4,7 @@ import { useProviders } from '@renderer/hooks/useProvider'
 import { getModelUniqId } from '@renderer/services/ModelService'
 import { KnowledgeBase } from '@renderer/types'
 import { nanoid } from 'nanoid'
-import { useCallback, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 const createInitialKnowledgeBase = (): KnowledgeBase => ({
@@ -44,6 +44,12 @@ export const useKnowledgeBaseForm = (base?: KnowledgeBase) => {
   const [newBase, setNewBase] = useState<KnowledgeBase>(base || createInitialKnowledgeBase())
   const { providers } = useProviders()
   const { preprocessProviders } = usePreprocessProviders()
+
+  useEffect(() => {
+    if (base) {
+      setNewBase(base)
+    }
+  }, [base])
 
   const selectedDocPreprocessProvider = useMemo(
     () => newBase.preprocessProvider?.provider,
